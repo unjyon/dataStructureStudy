3546fae9e66ca404371cc759bbde4cd9b8f45251
EunjungSong <unjyon.song@gmail.com>
2025-08-26 18:01:51 +0900

    feat: 2주차_강의요악중중

---
+++ stack.js
@@ -0,0 +1,30 @@
+class Stack {
+  arr = [];
+  push(value) {
+    return this.arr.push(value);
+  }
+  pop() {
+    return this.arr.pop();
+  }
+
+  // 프링글스 위에가 뭔지 알 수 있도록
+  // 프링글스의 맨 위는 배열의 마지막 요소
+  top() {
+    // return this.arr[this.arr.length - 1];에서 최신 문법은 아래와 같다.
+    return this.arr.at(-1);
+  }
+
+  get length() {
+    return this.arr.length;
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(5);
+stack.push(3);
+stack.push(7);
+stack.length;
+stack.pop();
+stack.top();
