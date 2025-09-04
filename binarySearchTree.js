947deca15d56befeecd9325f81f9289fa4987180
EunjungSong <ej.song@wconcept.co.kr>
2025-09-04 17:21:58 +0900

    feat:4강 length 구하기

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,5 +1,6 @@
 class BinarySearchTree {
   root = null;
+  length = 0;
 
   //insert 함수에 있는 재귀 함수 만들기
   #insert(node, value) {
@@ -11,6 +12,7 @@ class BinarySearchTree {
         this.#insert(node.left, value);
       } else {
         node.left = new Node(value);
+        this.length++;
       }
     } else {
       // 오른쪽 값이 있으면
@@ -18,6 +20,7 @@ class BinarySearchTree {
         this.#insert(node.right, value);
       } else {
         node.right = new Node(value);
+        this.length++;
       }
     }
   }
@@ -29,6 +32,7 @@ class BinarySearchTree {
     // 근데 만약 왼팔 오른팔이 없으면 거기다가 넣는다.
     if (!this.root) {
       this.root = new Node(value);
+      this.length++;
     } else {
       this.#insert(this.root, value);
       //숙제: 같은 값을 넣은 경우 에러처리(alert, throw)
@@ -74,23 +78,27 @@ class BinarySearchTree {
   #remove(node, value) {
     if (!node) {
       // 제거할 값이 bst에 존재하지 않는 경우
-      return false; // 지울 값이 존재 않하면 false return
+      return null; // 지울 값이 존재 않으면 null return
     }
     if (node.value === value) {
       // 자식 입장
       // 지울 값을 찾은 경우
       if (!node.left && !node.right) {
         // leaf 인 경우 - 부모에게 나를 잘라달라
+        this.length--;
         return null;
       } else if (!node.left) {
         // 왼팔이 없는 경우 - 오른팔을 끌어올린다
+        this.length--;
         return node.right;
       }
       if (!node.right) {
         // 오른팔이 없는 경우 - 왼팔을 끌어올린다
+        this.length--;
         return node.left;
       } else {
         // 양팔 다 있는 경우
+        this.length--;
         let exchange = node.left;
         //node.left.right.right.right 최대한 오른쪽으로 가야함
         while (exchange.right) {
@@ -121,10 +129,8 @@ class BinarySearchTree {
     // 2. leaf X, 왼팔이 없다 --> 오른팔을 끌어올린다
     // 3. leaf X, 오른팔이 없다 --> 왼팔을 끌어올린다
     // 4. leaf X, 양팔이 있다 --> 왼팔에서 가장 큰 애와 바꾼다, leaf 를 버린다.
-    const node = this.#remove(this.root, value);
-    if (node) {
-      this.root = node;
-    }
+    this.root = this.#remove(this.root, value);
+    return this.length; // 숙제로 length return
   }
 }
 
@@ -146,6 +152,13 @@ bst.insert(14);
 bst.insert(6);
 bst.insert(7);
 bst.insert(4);
-bst.insert(14);
+bst.insert(13);
 
-console.log(bst.insert(4));
+// console.log(bst.insert(4));
+console.log(bst.search(7));
+console.log(bst.search(5));
+bst.remove(8);
+console.log(bst.remove(15)); //false
+bst.remove(4);
+bst;
+bst.length;
